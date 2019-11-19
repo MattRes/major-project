0f3798c190e69f887d229c7f4d2e266a1c5a0067
MattRes <44241122+MattRes@users.noreply.github.com>
2019-11-19 14:10:12 -0600

    Created a class for main buttons and added the 2d collide library
    Button is currently unclickable but shows up.

--- major-project/sketch.js
+++ major-project/sketch.js
@@ -1,15 +1,47 @@
-// Project Title
-// Your Name
-// Date
+// RPG Game (Name Undecided)
+// Matthew Resendes
+// November 15th 2019 - 
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+let state; 
+let startButton;
+let buttonIndicator;
+
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  buttonIndicator = false;
+  textAlign(CENTER, CENTER);
+  rectMode(CENTER);
+  startButton = new Button(width/2 , height/2 , 100, 100, "blue", "Start");
 }
 
 function draw() {
-  background(220);
+  background(0);
+  startButton.display();
+  console.log(startButton.x, startButton.y);
+}
+
+class Button {
+  constructor(x,   y, width, height, color, text){
+    this.x = x;
+    this.y = y;
+    this.width = width; 
+    this.height = height;
+    this.color = color; 
+    this.text = text; 
+    }
+    display() {
+      fill(this.color);
+      rect(this.x, this.y, this.width, this.height);
+      fill("red");
+      text (this.text, this.x, this.y)
+  }
+}
+
+function displayStartScreen(){
+  background(66, 138, 245);
+
 }
