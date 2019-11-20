b47d3e464b49f399f984c99c8acb9c8f8cb08908
MattRes <44241122+MattRes@users.noreply.github.com>
2019-11-20 14:13:40 -0600

    Changed buttons from class to using the Clickables library and added some game states (game screen, and options)

--- major-project/sketch.js
+++ major-project/sketch.js
@@ -4,44 +4,66 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+
 let state; 
 let startButton;
-let buttonIndicator;
+let optionButton;
 
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  buttonIndicator = false;
+  state = "menu"
   textAlign(CENTER, CENTER);
   rectMode(CENTER);
-  startButton = new Button(width/2 , height/2 , 100, 100, "blue", "Start");
 }
 
 function draw() {
   background(0);
-  startButton.display();
-  console.log(startButton.x, startButton.y);
+  updateState();
 }
 
-class Button {
-  constructor(x,   y, width, height, color, text){
-    this.x = x;
-    this.y = y;
-    this.width = width; 
-    this.height = height;
-    this.color = color; 
-    this.text = text; 
-    }
-    display() {
-      fill(this.color);
-      rect(this.x, this.y, this.width, this.height);
-      fill("red");
-      text (this.text, this.x, this.y)
+function updateState(){
+  if (state === "menu"){
+    displayMenu(); 
+  }
+  if (state === "option"){
+    displayOptions();
   }
 }
+function displayMenu(){
+  background ("#524646");
+// Game text 
+  textSize(55);
+  text ("RPG Game", width/2, height/3 )
+  menuButtons();
+}
 
-function displayStartScreen(){
-  background(66, 138, 245);
+function menuButtons(){
+//Creates start button
+  startButton = new Clickable(width/2,height/2);
+  startButton.resize(200, 70);
+  startButton.color = "#b00e0e";
+  startButton.textSize = 20;
+  startButton.text = "New Game"
+  startButton.draw();
+  startButton.onHover = function(){
+    startButton.color = "#d11313";
+  }
+  
+
+//Creates option button
+  optionButton = new Clickable(width/2, height/2 + 100)
+  optionButton.resize(200, 70);
+  optionButton.color = "#b00e0e";
+  optionButton.textSize = 20;
+  optionButton.text = "Options"
+  optionButton.draw();
 
 }
+
+function displayOptions(){
+  background (0,0,0);
+}
+
+// keep player outside of text based grid
